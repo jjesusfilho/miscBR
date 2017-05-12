d305c17aca991db544e5b257fc79b012e65331a6
jjesusfilho <jjesusfilho@gmail.com>
2017-05-12 04:25:58 -0300

    Adicionando informações

---
+++ R/postal.R
@@ -0,0 +1,31 @@
+library(httr)
+#' Função postal
+#'
+#' This function return address based on Brazilian postal code
+#' @param vector with Brazilian postal codes
+#' @keywords postal code, cep
+#' @import httr
+#' @import stringr
+#' @return A data.frame with the addresses
+#' @export
+postal<-function(cep){
+  cep<-str_replace(cep,"\\D","")
+  cep<-str_pad(cep,8,side="left",pad="0")
+  cep<-as.character(cep)
+  df <- as.data.frame(setNames(replicate(6,numeric(0),
+                                         simplify = F),
+                               c("cep","tipoDeLogradouro","logradouro","bairro", "cidade", "estado")))
+  for(i in seq_along(cep)){
+    url<-paste0("http://correiosapi.apphb.com/cep/",cep)
+    a<-GET(url[i])
+    b<-content(a,as="parsed")
+    b<-t(unlist(b))
+    df<-rbind(df,b)
+  }
+  return(df)
+}
+
+#' @examples
+#' postal(02930000)
+#' postal(02929-020)
+#' postal("02846-000")
