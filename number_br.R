73cca002711b19e7e166777cf3978f89653867c0
jjesusfilho <jjesusfilho@gmail.com>
2017-05-12 04:45:47 -0300

    Atualização

---
+++ number_br.R
@@ -0,0 +1,20 @@
+#' Função number_br
+#'
+#' This converts numbers to English format.
+#' @param vector of numbers in format of character strings
+#' @keywords numbers
+#' @import stringr
+#' @return numeric vector
+#' @export
+
+number_br<-function(str){
+  str<-str_replace_all(str,"\\.","")
+  str<-str_replace_all(str,",",".")
+  str<-str_trim(str)
+  num<-as.numeric(str)
+  return(num)
+}
+#' @examples
+#' number_br(c("345.545,00","299,24"))
+
+
